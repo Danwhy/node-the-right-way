f46dd8bc43626892f1862beebc50d461590e1fc4
Danielwhyte <danwhy@gmail.com>
2015-07-31 16:38:42 +0100

    Example 17

---
+++ messaging/zmq-filer-req-loop.js
@@ -0,0 +1,20 @@
+"use strict";
+const zmq = require('zmq');
+const filename = process.argv[2];
+// create request endpoint
+const requester = zmq.socket('req');
+
+// handle replies from responder
+requester.on('message', function(data) {
+  console.dir("Received response: " + data);
+});
+
+requester.connect("tcp://localhost:5433");
+
+// send request for content
+for (let i = 1; i <= 3; i++) {
+  console.log("Sending request for " + filename);
+  requester.send(JSON.stringify({
+    path: filename
+  }));
+}
